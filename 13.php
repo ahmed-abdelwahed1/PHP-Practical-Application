62d96d233ee2d737e9e50c4dba916618a7329e2e
ahmed-abdelwahed1 <ahmedbinshehata@gmail.com>
2026-03-30 19:42:45 +0200

    Practical Application 13

---
+++ 13.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application 13</title>
+</head>
+<body>
+    <?php
+    // هذا تعليق لسطر واحد (Single line comment)
+
+    /*
+    هذا تعليق
+    يمتد لأكثر من سطر
+    (Multi-line comment)
+    */
+
+    echo "<h1>Hello, I'm Ahmed Abdelwahed<h2>";
+
+    ?>
+</body>
+</html>
