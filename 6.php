fe9f1c6cb9895ba094803cfac1da1d90aa3faae8
ahmed-abdelwahed1 <ahmedbinshehata@gmail.com>
2026-03-30 19:40:46 +0200

    Practical Application 6

---
+++ 6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application 6</title>
+</head>
+<body>
+    <?php
+    $a = 20;
+    $b = 3;
+
+    // إجراء العمليات الحسابية وطباعتها
+    echo $a + $b . "<br>";  // جمع (الناتج 23)
+    echo $a - $b . "<br>";  // طرح (الناتج 17)
+    echo $a * $b . "<br>";  // ضرب (الناتج 60)
+    echo $a / $b . "<br>";  // قسمة
+    echo $a % $b . "<br>";  // باقي القسمة
+    echo $a ** $b . "<br>"; // الأس (20 مرفوعة للأس 3)
+    ?>
+</body>
+</html>
