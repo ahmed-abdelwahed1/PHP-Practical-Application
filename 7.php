41564da65b17b05414b6f2fe5fb485a38b22eef1
ahmed-abdelwahed1 <ahmedbinshehata@gmail.com>
2026-03-30 19:41:06 +0200

    Practical Application 7

---
+++ 7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application 7</title>
+</head>
+<body>
+    <?php
+    $a = 20; // إسناد قيمة ابتدائية
+
+    $a += 5; // تعني $a = $a + 5 (ستصبح القيمة 25)
+    echo $a . "<br>";
+
+    $a -= 5; // تعني $a = $a - 5 (ستعود القيمة إلى 20)
+    echo $a . "<br>";
+    ?>
+</body>
+</html>
