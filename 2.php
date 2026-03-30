2c266966d5c9b4a2b1e2e231fc0ae0a6549d2c05
ahmed-abdelwahed1 <ahmedbinshehata@gmail.com>
2026-03-30 19:39:06 +0200

    Practical Application 2

---
+++ 2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application 2</title>
+</head>
+<body>
+    
+    <?php
+    // دالة date تستقبل معاملات الحروف لتحديد تنسيق الوقت والتاريخ (مثل السنة Y، الشهر m، اليوم d، إلخ)
+    echo "<p>Current date and time is " . date("Y-m-d H:i:s") . "</p>";
+    ?>
+
+</body>
+</html>
